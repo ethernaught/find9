1850f779564f48ed81c5f3d5bc483975c684b2b3
DrBrad <brad@bradeagle.com>
2025-04-30 22:46:39 -0600

    RPC Server

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,22 @@ mod database;
 mod rpc;
 mod utils;
 mod dns;
+mod unix_rpc;
 
+use std::io;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::os::unix::net::UnixListener;
 use crate::dns::Dns;
-
+use crate::unix_rpc::UnixRpc;
 //dig @127.0.0.1 -p 6767 net.unet
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut dns = Dns::new();
     dns.add_fallback(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1)), 53));
     dns.set_database("records.db");
     dns.start(6767).unwrap();
 
-
-    let listener = UnixListener::bind("/tmp/find9.sock").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-
-            },
-            Err(_) => {}
-        }
-    }
-
-
-
-
-
-    loop {}
+    let mut unix_rpc = UnixRpc::new()?;
+    unix_rpc.start()?.join().unwrap();
+    Ok(())
 }
---
+++ src/unix_rpc.rs
@@ -0,0 +1,62 @@
+use std::{fs, io, thread};
+use std::os::unix::net::UnixDatagram;
+use std::path::Path;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::JoinHandle;
+
+const UNIX_RPC_PATH: &str = "/tmp/find9.sock";
+
+pub struct UnixRpc {
+    server: Option<UnixDatagram>,
+    running: Arc<AtomicBool>
+}
+
+impl UnixRpc {
+
+    pub fn new() -> io::Result<Self> {
+        Ok(Self {
+            server: None,
+            running: Arc::new(AtomicBool::new(false))
+        })
+    }
+
+    pub fn start(&mut self) -> io::Result<JoinHandle<()>> {
+        if self.is_running() {
+            return Err(io::Error::new(io::ErrorKind::Other, "Server is already running"));
+        }
+
+        self.running.store(true, Ordering::Relaxed);
+        if Path::new(UNIX_RPC_PATH).exists() {
+            fs::remove_file(UNIX_RPC_PATH).unwrap();
+        }
+
+        self.server = Some(UnixDatagram::bind(UNIX_RPC_PATH)?);
+        self.server.as_ref().unwrap().set_nonblocking(true)?;
+
+        Ok(thread::spawn({
+            let server = self.server.as_ref().unwrap().try_clone()?;
+            let running = Arc::clone(&self.running);
+            move || {
+                let mut buf = [0u8; 65535];
+
+                while running.load(Ordering::Relaxed) {
+                    match server.recv_from(&mut buf) {
+                        Ok((size, src_addr)) => {
+                            println!("Received {} bytes from {:?}", size, src_addr);
+                        }
+                        Err(_) => {}
+                    }
+                }
+            }
+        }))
+    }
+
+    pub fn stop(&self) {
+        self.running.store(false, Ordering::Relaxed);
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.running.load(Ordering::Relaxed)
+    }
+}
