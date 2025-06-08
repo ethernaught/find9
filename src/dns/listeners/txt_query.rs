88dbf49933f9cb59fe4f8945e80ad846ff094d70
DrBrad <brad@bradeagle.com>
2025-06-08 14:13:49 -0600

    Cleanup

--- src/dns/listeners/txt_query.rs
+++ src/dns/listeners/txt_query.rs
@@ -32,31 +32,4 @@ pub fn on_txt_query(zones: &Arc<RwLock<Zone>>) -> impl Fn(&mut QueryEvent) -> io
 
         Ok(())
     }
-    //move |event| {
-        /*
-        //let is_bogon = is_bogon(message.get_origin().unwrap());//if  { "network < 2" } else { "network > 0" };
-        let is_bogon = "network < 2";
-        let name = event.get_query().get_name().to_lowercase();
-        let class = event.get_query().get_dns_class();
-
-        let records = database.get(
-            "txt",
-            Some(vec!["class", "ttl", "content", "network"]),
-            Some(format!("class = {} AND name = '{}' AND {}", class.get_code(), name, is_bogon).as_str()),
-        );
-
-        if records.is_empty() {
-            return Err(io::Error::new(io::ErrorKind::Other, "Document not found"));
-        }
-
-        for record in records {
-            let ttl = record.get("ttl").unwrap().parse::<u32>().unwrap();
-            let content = record.get("content").unwrap().as_str();
-
-            event.add_answer(&name, Box::new(TxtRecord::new(class, false, ttl, vec![content.to_string()])));
-        }
-        */
-
-    //    Ok(())
-    //}
 }
