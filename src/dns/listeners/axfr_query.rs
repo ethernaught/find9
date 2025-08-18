2d034252addc26d85e4316b2c836b07a1b0b9ebd
DrBrad <brad@bradeagle.com>
2025-08-18 02:59:34 -0600

    Problem records found

--- src/dns/listeners/axfr_query.rs
+++ src/dns/listeners/axfr_query.rs
@@ -40,7 +40,6 @@ pub fn on_axfr_query(zones: &Arc<RwLock<Zone>>) -> impl Fn(&mut QueryEvent) -> R
                                 event.add_answer(&format!("{n}{name}"), record.clone());
                                 event.add_answer(&format!("{n}{name}"), record.clone());
 
-                                /*
                                 event.add_answer(&format!("{n}{name}"), record.clone());
                                 event.add_answer(&format!("{n}{name}"), record.clone());
                                 event.add_answer(&format!("{n}{name}"), record.clone());
@@ -61,7 +60,6 @@ pub fn on_axfr_query(zones: &Arc<RwLock<Zone>>) -> impl Fn(&mut QueryEvent) -> R
                                 event.add_answer(&format!("{n}{name}"), record.clone());
                                 event.add_answer(&format!("{n}{name}"), record.clone());
                                 event.add_answer(&format!("{n}{name}"), record.clone());
-                                */
 
                             }
                         }
